626cc54cfcf80c4b0e4cdcd4462a958f3e608a54
ensmorgastack <212952397+ensmorgastack@users.noreply.github.com>
2025-06-05 12:47:03 +0200

    add more languages to translate from

--- app.py
+++ app.py
@@ -2,6 +2,39 @@ import streamlit as st
 from textblob import TextBlob
 from deep_translator import GoogleTranslator
 
+language_codes = {
+    "🇧🇬 Bulgarian": "bg",
+    "🇨🇳 Chinese (Simplified)": "zh-CN",
+    "🇨🇿 Czech": "cs",
+    "🇩🇰 Danish": "da",
+    "🇳🇱 Dutch": "nl",
+    "🇬🇧 English": "en",
+    "🇪🇪 Estonian": "et",
+    "🇫🇮 Finnish": "fi",
+    "🇫🇷 French": "fr",
+    "🇩🇪 German": "de",
+    "🇬🇷 Greek": "el",
+    "🇭🇺 Hungarian": "hu",
+    "🇮🇩 Indonesian": "id",
+    "🇮🇹 Italian": "it",
+    "🇯🇵 Japanese": "ja",
+    "🇰🇷 Korean": "ko",
+    "🇱🇻 Latvian": "lv",
+    "🇱🇹 Lithuanian": "lt",
+    "🇳🇴 Norwegian (Bokmål)": "no",
+    "🇵🇱 Polish": "pl",
+    "🇵🇹 Portuguese": "pt",
+    "🇷🇴 Romanian": "ro",
+    "🇷🇺 Russian": "ru",
+    "🇸🇰 Slovak": "sk",
+    "🇸🇮 Slovenian": "sl",
+    "🇪🇸 Spanish": "es",
+    "🇸🇪 Swedish": "sv",
+    "🇹🇷 Turkish": "tr",
+    "🇺🇦 Ukrainian": "uk"
+}
+
+
 def analyze_sentiment(text):
     blob = TextBlob(text)
     polarity = blob.sentiment.polarity
@@ -12,16 +45,26 @@ def analyze_sentiment(text):
     else:
         return "Neutral 😐"
 
-def translate_to_swedish(text):
-    return GoogleTranslator(source='en', target='sv').translate(text)
+def translate(text, from_lang, to_lang):
+    return GoogleTranslator(source = from_lang, target = to_lang).translate(text)
 
-st.title("NordMood: Emotional Translator to Swedish")
+st.title("NordMood: Sentiment Translator to Swedish")
 
-user_input = st.text_input("Enter a sentence in English")
+option = st.selectbox(
+    "Pick language",
+    (language_codes),
+)
+
+user_input = st.text_input(f"Enter a sentence in {option[2:]}")
 
 if user_input:
-    sentiment = analyze_sentiment(user_input)
-    translation = translate_to_swedish(user_input)
+    from_language = language_codes.get(option)
+    phrase = user_input
+    if from_language != "en":
+        phrase = translate(user_input, from_language, 'en')
+
+    sentiment = analyze_sentiment(phrase)
+    translation = translate(user_input, language_codes.get(option), 'sv')
     
     st.markdown(f"*Sentiment:* {sentiment}")
     st.markdown(f"*Swedish Translation:* {translation}")
